a0493f266228c194cfe2952ec174ad7d58da011d
chrisvm <christian.velez5@upr.edu>
2015-09-14 00:13:50 -0400

    set correct jade name

--- routes/stop.js
+++ routes/stop.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('top_layout', { head_title: 'Stop Main Page Mock-Up' });
+    res.render('main_page', { head_title: 'Stop Main Page Mock-Up' });
 });
 
 module.exports = router;
