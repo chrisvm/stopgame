e7ecc8bc0422618c2b46fedb48a8778501eec8ad
chrisvm <christian.velez5@upr.edu>
2015-09-14 23:58:37 -0400

    more expressive params

--- routes/stop.js
+++ routes/stop.js
@@ -2,8 +2,8 @@ var express = require('express');
 
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.render('main_page', { head_title: 'Stop Main Page Mock-Up' });
+router.get('/', function(request, response, next) {
+    response.render('main_page', { head_title: 'Stop Main Page Mock-Up' });
 });
 
 module.exports = router;
