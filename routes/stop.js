a162427069c5db26f02e99bd44e4ddc40f9fcd8f
chrisvm <christian.velez5@upr.edu>
2015-09-17 23:19:36 -0400

    uses toggled option

--- routes/stop.js
+++ routes/stop.js
@@ -3,7 +3,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(request, response, next) {
-    response.render('main_page', { head_title: 'Stop Main Page Mock-Up' });
+    response.render('main_page', {
+        head_title: 'Stop Main Page Mock-Up',
+        toggled: false
+    });
 });
 
 router.get('/first_page', function(request, response, next) {
