5a17ee244eaa9465f513e3180e13a68042a74d2a
chrisvm <christian.velez5@upr.edu>
2015-09-19 16:52:14 -0400

    fixed socket.io port error

--- public/js/first_page.js
+++ public/js/first_page.js
@@ -1,9 +1,7 @@
 /**
  * Created by benjamin on 09-19-15.
  */
-
 function checkUser(){
     var input_text = document.getElementById('username').value;
-    var socket = io.connect('http://localhost');
-    alert("here");
+    var socket = io.connect('http://localhost:3000');
 };
