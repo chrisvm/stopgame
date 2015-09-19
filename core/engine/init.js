54b7657916a328105ea3549227bd97844153b44d
chrisvm <christian.velez5@upr.edu>
2015-09-19 16:21:19 -0400

    engine.init module

--- core/engine/init.js
+++ core/engine/init.js
@@ -1,3 +1,21 @@
-/**
- * Created by chrisvm on 9/19/15.
+/** init.js
+ * This file exports the init object, which holds all the init methods for the
+ * start of the app.
  */
+
+// the init object
+var init = {};
+
+/**
+ * Attaches all the handlers to the socketio object
+ *
+ * @param socket.io.Server io instance of a socket io server to add the handlers
+ */
+init.socketio = function (io) {
+    io.on('connection', function (socket) {
+        console.log('conection started');
+    });
+};
+
+
+module.exports = init;
