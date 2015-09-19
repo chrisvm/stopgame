4bd0f523c534e4d2136acd7f8da050c2d8be3415
BenjaBoy <patonacevedo@gmail.com>
2015-09-19 16:22:41 -0400

    function checkUser now in first_page.js

---
+++ public/js/first_Page.js
@@ -0,0 +1,10 @@
+/**
+ * Created by benjamin on 09-19-15.
+ */
+
+function checkUser(){
+    var input_text = document.getElementById('username');
+    input_text.disable =  true
+
+    return false;
+};
