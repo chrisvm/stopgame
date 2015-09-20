d57dfb63762916e2118aad9038c3e78fb0ba8e84
chrisvm <christian.velez5@upr.edu>
2015-09-20 04:47:01 -0400

    set post handler

--- routes/stop.js
+++ routes/stop.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 router.get('/', function(request, response, next) {
     var options = {
-        head_title: 'Stop Main Page Mock-Up',
+        head_title: 'Stop Main Page',
         toggled: true,
         sidebar_content: jade.renderFile("views/sidebar.jade"),
         content: jade.renderFile("views/first_page.jade")
@@ -14,4 +14,20 @@ router.get('/', function(request, response, next) {
     response.render('top_layout', options);
 });
 
+router.post('/', function (request, response) {
+    if (!'username' in request.body) {
+        var locals = {
+            "message": "Error on sent POST body",
+            "status": "username parameter not set"
+        };
+        response.render("error", locals);
+    }
+
+    // get username
+    var username = request.body.username;
+
+    // render game_page
+    var games_page = jade.renderFile("views/second_page.jade");
+    response.html(games_page);
+});
 module.exports = router;
