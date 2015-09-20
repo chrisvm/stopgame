9548dc659e71a07bede6a6b72ae9f5b2d3ff23c1
chrisvm <christian.velez5@upr.edu>
2015-09-20 13:10:26 -0400

    set get user on load

--- public/js/games_page.js
+++ public/js/games_page.js
@@ -1,4 +1,11 @@
-function join_click(){
-    alert(this.id);
-    return false;
-}
+socket.on("room all_users_response", function (opts) {
+    if (opts.status.code == 200) {
+        var $games_list = $("#games-list");
+        $games_list.html(
+            SessionList({sessions: opts.rooms});
+        );
+    }
+});
+socket.emit("room all_users", {
+    "username": socket.username,
+});
