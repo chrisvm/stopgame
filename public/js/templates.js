a8f7c1688caf0ed0e89bb6653fcad5cadce4c376
chrisvm <christian.velez5@upr.edu>
2015-09-20 13:08:36 -0400

    added template utils

---
+++ public/js/templates.js
@@ -0,0 +1,21 @@
+var jade = require("jade");
+
+var templates = [
+    {
+        "name": "SessionList",
+        "filepath": "../views/session_list.jade"
+    }
+];
+
+function init_templates () {
+    for (var x = 0; x < templates.length; x++) {
+        var t = templates[x];
+        var jscode = jade.compileFileClient(t.filepath, {name: t.name});
+        // todo: finish the template compilation 
+        var script = jade.render("script!= source", {
+            "self":
+        })
+        var $script = $("<script/>").html(jscode);
+        $("body").append($script);
+    }
+}
