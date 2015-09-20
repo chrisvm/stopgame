d9d09a1d649428831ccede4740f510fb7352aeba
chrisvm <christian.velez5@upr.edu>
2015-09-19 23:55:56 -0400

    finished auth_username method

--- core/engine/engine_user.js
+++ core/engine/engine_user.js
@@ -9,9 +9,26 @@ user.auth_username = function (socket, opts) {
     var datadb = user.datadb;
 
     // get users
-    datadb.models.User.find(function (err, users) {
+    datadb.models.User.findOne({ "name": opts.username }, function (err, user) {
         if (err) return console.error(err);
-        console.log(users);
+
+        // create respone status
+        var status = {};
+        if (user == null) {
+            status.code = 200;
+            status.short = 'ALL_OK';
+            status.error = '';
+        } else {
+            status.code = 300;
+            status.short = 'NOT_OK';
+            status.error = 'User not found in db';
+        }
+
+        var response = {
+            "status": status,
+            "username": opts.username
+        };
+        socket.emit("user auth_username_response", response);
     });
 };
 
