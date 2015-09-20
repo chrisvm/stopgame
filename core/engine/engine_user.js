7a211bf2673f301fb68f77870f1f503fb546928f
chrisvm <christian.velez5@upr.edu>
2015-09-20 00:30:28 -0400

    fixed user save on db

--- core/engine/engine_user.js
+++ core/engine/engine_user.js
@@ -9,7 +9,7 @@ user.auth_username = function (socket, opts) {
     var datadb = user.datadb;
 
     // get users
-    datadb.models.User.findOne({ "name": opts.username }, function (err, user) {
+    datadb.models.User.findOne({ "username": opts.username }, function (err, user) {
         if (err) return console.error(err);
 
         // create respone status
@@ -18,12 +18,17 @@ user.auth_username = function (socket, opts) {
             status.code = 200;
             status.short = 'ALL_OK';
             status.error = '';
+            
+            // save the new user
+            var new_user = new datadb.models.User({ "username": opts.username });
+            new_user.save();
         } else {
             status.code = 300;
             status.short = 'NOT_OK';
-            status.error = 'User not found in db';
+            status.error = 'User found in db';
         }
 
+        // send response
         var response = {
             "status": status,
             "username": opts.username
