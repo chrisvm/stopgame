c9f2c7ffef980e4e342526700d454414a738f1fc
chrisvm <christian.velez5@upr.edu>
2015-09-20 07:03:45 -0400

    added room schema

--- core/datadb/models_schema.js
+++ core/datadb/models_schema.js
@@ -1,10 +1,31 @@
+var mongoose = require('mongoose');
+
 var UserSchema = {
     "name": "User",
-    "schema": {
+    "schema": new mongoose.Schema({
         "username": String
-    }
+    })
+};
+
+var RoomStateSchema = {
+    "name": "RoomState",
+    "schema": new Schema({
+
+    })
+};
+
+var RoomSchema = {
+    "name": "Room",
+    "schema": new mongoose.Schema({
+        "name": String,
+        "users": [UserSchema.schema],
+        "ready": [UserSchema.schema],
+        "state": RoomState
+    })
 };
 
 module.exports = [
-    UserSchema
+    UserSchema,
+    RoomStateSchema,
+    RoomSchema
 ];
