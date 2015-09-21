37b65f146718653e2e348b5f6644f8ef5f66b59b
BenjaBoy <patonacevedo@gmail.com>
2015-09-21 00:37:37 -0400

    game_page.js fixed sintax error

--- public/js/games_page.js
+++ public/js/games_page.js
@@ -2,10 +2,11 @@ socket.on("room all_users_response", function (opts) {
     if (opts.status.code == 200) {
         var $games_list = $("#games-list");
         $games_list.html(
-            SessionList({sessions: opts.rooms});
+            SessionList({sessions: opts.rooms})
         );
     }
 });
+
 socket.emit("room all_users", {
     "username": socket.username,
 });
