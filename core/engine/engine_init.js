5c1f3d5caddec045402b747eb91ccf2a582855eb
chrisvm <christian.velez5@upr.edu>
2015-09-23 03:08:18 -0400

    added new engine calls

--- core/engine/engine_init.js
+++ core/engine/engine_init.js
@@ -16,12 +16,21 @@ var init = {
 init.socketio = function (io) {
     console.log("Initing socket.io handlers");
 
+    // todo: make this into a engine_conf file
     io.on('connection', function (socket) {
         console.log('client connected');
 
         // set the socket methods
-        socket.on('user auth_username', function(data){
-            init.engine.user.auth_username(socket, data);
+        socket.on('user auth_username', function(opts){
+            init.engine.user.auth_username(socket, opts);
+        });
+
+        socket.on('room all_rooms', function (opts) {
+            init.engine.room.all_rooms(socket, opts);
+        });
+
+        socket.on('room create_room', function (opts) {
+            init.engine.room.create_room(socket, opts);
         });
     });
 
