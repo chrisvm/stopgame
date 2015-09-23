ff5764758c0b43300803c6bb2ff2096cf75a175b
BenjaBoy <patonacevedo@gmail.com>
2015-09-23 03:23:41 -0400

    added util.js, changed set_alert to util.js

--- public/js/first_page.js
+++ public/js/first_page.js
@@ -8,9 +8,12 @@ function checkUser() {
     $username_textbox.attr("disabled", true);
     $go_button.attr("disabled", true);
 
+    var alertData = {"btn"  : $go_button.attr('id'),
+                     "inp": $username_textbox.attr('id')};
+
     if (username == '') {
         // set warning to form-group
-        set_alert('error', "Empty Username");
+        set_alert('error', "Empty Username", alertData);
 
         // disable disable
         setTimeout(function () {
@@ -40,8 +43,7 @@ function checkUser() {
     // set the callback
     socket.on('user auth_username_response', function (opts) {
         // if already set username, return
-        if (socket.username != null) return;
-
+        if (socket.username != null)return;
         if (opts.status.code == 200) {
             // if user not found
             // add username to socket
@@ -80,7 +82,7 @@ function checkUser() {
 
         } else if (opts.status.code == 300) {
             // if user already in server
-            set_alert("error", "Username taken");
+            set_alert("error", "Username taken", alertData);
             setTimeout(function () {
                 $username_textbox.attr("disabled", false);
                 $go_button.attr("disabled", false);
@@ -89,40 +91,5 @@ function checkUser() {
     });
 }
 
-function set_alert(type, tooltip_message) {
-    var $go_button = $("#go_btn");
-    var $feedback, addedClasses;
 
-    if (type == 'alert') {
-        // set warning to form-group
-        addedClasses = "has-warning has-feedback";
-        $feedback = $('<span class="glyphicon glyphicon-warning-sign form-control-feedback" aria-hidden="true"></span>');
-    } else if (type == 'error') {
-        addedClasses = "has-error has-feedback";
-        $feedback = $('<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>');
-    } else {
-        return;
-    }
-
-    // add alert clases
-    var $input_group = $("#input_group");
-    $input_group.addClass(addedClasses);
-
-    $go_button.tooltip({
-        "title": tooltip_message,
-        "placement": "bottom",
-        "trigger": "manual"
-    });
-    $go_button.tooltip("show");
-
-    // set icon feedback
-    $input_group.append($feedback);
-
-    // set disappereance callback
-    setTimeout(function () {
-        $go_button.tooltip("hide");
-        $feedback.remove();
-        $input_group.removeClass(addedClasses);
-    }, 1500);
-}
 
---
+++ public/js/utils.js
@@ -0,0 +1,39 @@
+function set_alert(type, tooltip_message, alert_data) {
+    var $go_button = $(alert_data.btn);
+    var $input = $(alert_data.inp);
+    var $feedback, addedClasses;
+
+    if (type == 'alert') {
+        // set warning to form-group
+        addedClasses = "has-warning has-feedback";
+        $feedback = $('<span class="glyphicon glyphicon-warning-sign form-control-feedback" aria-hidden="true"></span>');
+    } else if (type == 'error') {
+        addedClasses = "has-error has-feedback";
+        $feedback = $('<span class="glyphicon glyphicon-remove form-control-feedback" aria-hidden="true"></span>');
+    } else {
+        return;
+    }
+
+    // add alert clases
+    var $input_group = $("#input-group");
+    $input_group.addClass(addedClasses);
+
+    $go_button.tooltip({
+        "title": tooltip_message,
+        "placement": "bottom",
+        "trigger": "manual"
+    });
+    $go_button.tooltip("show");
+
+    // set icon feedback
+    $input_group.append($feedback);
+
+    // set disappereance callback
+    setTimeout(function () {
+        $go_button.tooltip("hide");
+        $feedback.remove();
+        $input_group.removeClass(addedClasses);
+        $input.attr("disabled", false);
+        $go_button.attr("disabled", false);
+    }, 1500);
+}
