2d51ae7d474aefa797f79ae71dfc34af92791a3d
chrisvm <christian.velez5@upr.edu>
2015-09-23 03:59:32 -0400

    fixed template creation

--- public/js/templates.js
+++ public/js/templates.js
@@ -7,16 +7,12 @@ var templates = [
     }
 ];
 
-var script_template = "<script> !=source </script>";
-
 function get_templates () {
     var ret = "";
     for (var x = 0; x < templates.length; x++) {
         var t = templates[x];
         var jscode = jade.compileFileClient(t.filepath, {name: t.name});
-        var script = jade.render(script_template, { "self": {
-            "source": jscode
-        }});
+        var script = "<script>" + jscode + "</script>";
         ret += script;
     }
     return ret;
