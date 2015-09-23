a40601629489ca74b9663575cae47f881ac001ca
chrisvm <christian.velez5@upr.edu>
2015-09-23 01:28:10 -0400

    updated server ip

--- core/datadb/datadb.js
+++ core/datadb/datadb.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 // db constants
-MONGODB_SERVER = 'mongodb://52.88.157.123/stop';
+MONGODB_SERVER = 'mongodb://159.203.85.83/stop';
 
 function DataDB() {
     // set mongoose reference
