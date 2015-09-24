e435455a249ef048a85405480aa2497951333d19
chrisvm <christian.velez5@upr.edu>
2015-09-23 20:35:10 -0400

    added single connection

--- core/datadb/db_conn.js
+++ core/datadb/db_conn.js
@@ -1,3 +1,18 @@
-/**
- * Created by chrisvm on 9/23/15.
- */
+var mongoose = require('mongoose');
+
+// db constants
+MONGO_CREDENTIALS = {
+    user: "stopgame_server",
+    pass: "St0pGam3",
+    url: "ds051953.mongolab.com:51953/heroku_3vs0lpzq"
+};
+
+function get_connection() {
+    // connect to the server
+    var mongo_server = "mongodb://{user}:{pass}@{url}".format(MONGO_CREDENTIALS);
+    console.log("Connecting to " + mongo_server);
+    mongoose.connect(mongo_server);
+    return mongoose;
+}
+
+module.exports = get_connection();
